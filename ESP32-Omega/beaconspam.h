a166db0f92e7f63e819a458ea0af810c197765e6
Wraith <spaceboy1112@gmail.com>
2026-07-18 23:28:31 +0330

    Update beaconspam.h

--- ESP32-Omega/beaconspam.h
+++ ESP32-Omega/beaconspam.h
@@ -9,19 +9,18 @@ int beaconCount = 0;
 int maxBeacons = 100;
 
 uint8_t beaconPacket[128] = {
-  0x80, 0x00, 0x00, 0x00,  // Frame control
-  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,  // Broadcast
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // SRC
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // BSSID
-  0xC0, 0x6C,  // Sequence
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // Timestamp
-  0x64, 0x00,  // Beacon interval
-  0x01, 0x04,  // Capability
-  0x00  // SSID tag (length follows)
+  0x80, 0x00, 0x00, 0x00,
+  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0xC0, 0x6C,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x64, 0x00,
+  0x01, 0x04,
+  0x00
 };
 
 const char* ssidChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-const char* ssidPrefix = "ESP_";
 
 void startBeaconSpam(int count) {
   beaconSpamActive = true;
@@ -47,27 +46,22 @@ void runBeaconSpam() {
     return;
   }
   
-  // Random channel
   int channel = random(1, 13);
   esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
   
-  // Randomize MAC
   for (int i = 0; i < 6; i++) {
     uint8_t b = random(256);
-    beaconPacket[10 + i] = b;  // SRC
-    beaconPacket[16 + i] = b;  // BSSID
+    beaconPacket[10 + i] = b;
+    beaconPacket[16 + i] = b;
   }
   
-  // Random SSID (6 chars with prefix)
-  beaconPacket[37] = 6;  // SSID length
+  beaconPacket[37] = 6;
   for (int i = 0; i < 6; i++) {
     beaconPacket[38 + i] = ssidChars[random(36)];
   }
   
-  // Channel in supported rates
   beaconPacket[56] = channel;
   
-  // Send 3 times for reliability
   for (int i = 0; i < 3; i++) {
     esp_wifi_80211_tx(WIFI_IF_AP, beaconPacket, sizeof(beaconPacket), false);
   }
